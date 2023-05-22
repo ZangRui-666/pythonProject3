dc3ea569102fc31d7f6b9c56c3fb163695f7719e
raymonadzang <19251017@life.hkbu.edu.hk>
2023-05-22 15:22:23 +0800

    fix

--- test2.py
+++ test2.py
@@ -3,12 +3,26 @@ import osmnx as ox
 import momepy as mm
 import matplotlib.pyplot as plt
 import numpy as np
+import contextily as cx
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 # from colorspacious import cspace_converter
-
+import mplleaflet
+import tilemapbase
 gdf = gpd.read_file('/Users/patrick/Desktop/hkbu-college/doc/year3/sem1/econ3105/pythonProject3/output_test1.shp')
-gdf.plot('value', cmap='OrRd',legend=True)
+print(gdf.crs)
+gdf.to_crs(3857, inplace=True)
+tilemapbase.start_logging()
+tilemapbase.init(create=True)
 
+ax = gdf.plot('value', cmap='OrRd', legend=True)
+cx.add_basemap(ax)
+print('done')
 plt.show()
+
+# plt.plot(gdf)
+print(gdf.crs)
+print(gdf.info())
+mplleaflet.show()
+
 print('')
