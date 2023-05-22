6d407635caade855c35271fae0687ba1894c45b1
raymonadzang <19251017@life.hkbu.edu.hk>
2023-05-23 00:37:59 +0800

    fix

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 from pynkdv.PyNKDV import *
-
+import pandas as pd
 setPath(['/Users/patrick/Library/Application Support/QGIS/QGIS3/profiles/default/python/plugins/ProcessX', '/Users/patrick/opt/anaconda3/envs/pynkdv/share/qgis/python', '/Users/patrick/Library/Application Support/QGIS/QGIS3/profiles/default/python', '/Users/patrick/Library/Application Support/QGIS/QGIS3/profiles/default/python/plugins', '/Users/patrick/opt/anaconda3/envs/pynkdv/share/qgis/python/plugins', '/Users/patrick/opt/anaconda3/envs/pynkdv/share/qgis/python/plugins', '/Users/patrick/opt/anaconda3/envs/pynkdv/share/qgis/python', '/Users/patrick', '/Users/patrick/opt/anaconda3/envs/pynkdv/lib/python39.zip', '/Users/patrick/opt/anaconda3/envs/pynkdv/lib/python3.9', '/Users/patrick/opt/anaconda3/envs/pynkdv/lib/python3.9/lib-dynload', '/Users/patrick/opt/anaconda3/envs/pynkdv/lib/python3.9/site-packages', '/Users/patrick/Library/Application Support/QGIS/QGIS3/profiles/default/python'])
-map_data = map_road_network('/Users/patrick/Desktop/fypProject/Datasets_Yun/San_Francisco_clean.csv')
-model = PyNKDV(map_data, bandwidth=500, lixel_size=20, num_threads=8)
-results = model.compute()
-output(results, 'output_test1')
+
+df1 = pd.read_csv('/Users/patrick/Desktop/fypProject/dataset/Gainesville_Crime_Responses.csv',sep=' ', header=None, names=['x','y'])
+print(df1.info())
+df2 = df1[(df1['x'] != 0.0) & (df1['y'] != 0.0)]
+df2.to_csv('/Users/patrick/Desktop/fypProject/dataset/Gainesville_Crime_Responses.csv', sep=' ', header=None, index=False)
+map_data = map_road_network('/Users/patrick/Desktop/fypProject/dataset/Gainesville_Crime_Responses.csv')
+
+# model = PyNKDV(map_data, bandwidth=500, lixel_size=20, num_threads=8)
+# results = model.compute()
+# output(results, 'output_test1')
--- test2.py
+++ test2.py
@@ -9,16 +9,28 @@ import matplotlib.pyplot as plt
 # from colorspacious import cspace_converter
 import mplleaflet
 import tilemapbase
-gdf = gpd.read_file('/Users/patrick/Desktop/hkbu-college/doc/year3/sem1/econ3105/pythonProject3/output_test1.shp')
+# gdf = gpd.read_file('/Users/patrick/Desktop/hkbu-college/doc/year3/sem1/econ3105/pythonProject3/output_test1.shp')
+gdf = gpd.GeoDataFrame.from_file('/Users/patrick/Desktop/hkbu-college/doc/year3/sem1/econ3105/pythonProject3/output_test1.shp')
+
 print(gdf.crs)
-gdf.to_crs(3857, inplace=True)
+# gdf.to_crs(3857, inplace=True)
+gdf.to_crs(4326, inplace=True)
 
 #ax = gdf.plot('value', cmap='OrRd', scheme='equal_interval', legend=True, alpha=1, linewidth=1.5)
 # alpha scheme cmap
-plt.figure(figsize=(1000, 1000))
-ax = gdf.plot(column='value', cmap='OrRd', figsize=(1000, 1000),legend=True, linewidth=1.5)
-cx.add_basemap(ax)
+# ax = gdf.plot(column='value', scheme='quantiles', cmap='OrRd', figsize=(200, 200),legend=True, linewidth=1.5)
+m = gdf.explore(column='value', cmap='OrRd')
+m.save('test_active_map')
+#ax = gdf.plot(column='value', cmap='OrRd')
+
+# plt.savefig('test_fig')
+# mplleaflet.show(fig=ax.figure, crs=gdf.crs, path='sgmap.html')
+# mplleaflet.show(fig=ax.figure, crs=gdf.crs, path='sgmap1.html')
+
+
+# cx.add_basemap(ax)
 print('done')
+# plt.savefig('test_fig')
 plt.show()
 
 # plt.plot(gdf)
